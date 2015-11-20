40fd78878555109ea437c76f0d842195d5297288
Ben Hodeda <benhodeda@gmail.com>
2015-11-20 04:14:14 +0200

    jknk

--- app.js
+++ app.js
@@ -7,9 +7,10 @@ var routes = require('./routes/index');
 
 var app = express();
 
-app.all('/', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+app.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 
