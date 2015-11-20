35c1f2f78ba0c7b8482049ce10b7f2c791f92eeb
Ben Hodeda <benhodeda@gmail.com>
2015-11-20 04:09:24 +0200

    jknk

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ var routes = require('./routes/index');
 
 var app = express();
 
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
